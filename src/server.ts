0726ea20e3507d0e138a45beced1a4a04dbd393c
dani9oo <dani9oo.dev@gmail.com>
2020-12-02 00:44:24 -0600

    Added skill controller

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { config } from "dotenv";
 import App from "./app";
 import InquiryController from "./inquiry/inquiry.controller";
+import SkillController from "./skill/skill.controller";
 
 config();
 
 const app = new App([
-    new InquiryController
+    new InquiryController,
+    new SkillController
 ]);
 
 app.listen();
