cd9801012253eda93212cfc13d713f4888f424e0
dani9oo <dani9oo.dev@gmail.com>
2020-12-03 00:03:58 -0600

    Renamed skill dto to fix typo

---
+++ src/skill/skill.dto.ts
@@ -0,0 +1,41 @@
+import { IsDivisibleBy, IsMongoId, IsNumber, IsString, Max, Min, IsOptional } from 'class-validator';
+
+
+class SkillDTO {
+    @IsString()
+    public language: string;
+    
+    @IsNumber()
+    @IsDivisibleBy(0.5)
+    @Min(0)
+    @Max(5)
+    public expertise: number;
+
+    @IsString()
+    public icon: string;
+}
+
+class ModifySkillDTO {
+    @IsMongoId()
+    public _id: string;
+
+    @IsString()
+    @IsOptional()
+    public language: string;
+    
+    @IsNumber()
+    @IsDivisibleBy(0.5)
+    @Min(0)
+    @Max(5)
+    @IsOptional()
+    public expertise: number;
+
+    @IsString()
+    @IsOptional()
+    public icon: string;
+}
+
+export {
+    SkillDTO,
+    ModifySkillDTO
+};
