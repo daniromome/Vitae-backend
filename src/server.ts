192693dd69844b4c18950b03d9df16635b7e285c
dani9oo <dani9oo.dev@gmail.com>
2020-12-03 00:05:13 -0600

    Added project module controller

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import { config } from "dotenv";
 import App from "./app";
 import InquiryController from "./inquiry/inquiry.controller";
+import ProjectController from "./project/project.controller";
 import SkillController from "./skill/skill.controller";
 
 config();
 
 const app = new App([
     new InquiryController,
-    new SkillController
+    new SkillController,
+    new ProjectController
 ]);
 
 app.listen();
